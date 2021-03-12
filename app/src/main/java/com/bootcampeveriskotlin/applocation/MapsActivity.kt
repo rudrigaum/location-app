c50a070016404fb38d241ba7d4725d142dad91ba
rudrigocerqueira <rodrigocerqueirareis@gmail.com>
2021-03-12 09:05:51 -0300

    User marker code on the map

--- app/src/main/java/com/bootcampeveriskotlin/applocation/MapsActivity.kt
+++ app/src/main/java/com/bootcampeveriskotlin/applocation/MapsActivity.kt
@@ -1,6 +1,9 @@
 package com.bootcampeveriskotlin.applocation
 
 import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.location.Address
+import android.location.Geocoder
 import android.location.Location
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -11,8 +14,11 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import java.util.*
 
 class MapsActivity : AppCompatActivity(),
                      OnMapReadyCallback,
@@ -64,15 +70,43 @@ class MapsActivity : AppCompatActivity(),
 
         map.isMyLocationEnabled = true
 
-        fusedLocationClient.lastLocation.addOnSuccessListener(this){ location ->
+        map.mapType = GoogleMap.MAP_TYPE_HYBRID
 
+        fusedLocationClient.lastLocation.addOnSuccessListener(this){ location ->
             if(location != null){
                 lastLocation = location
                 val currentLatLng = LatLng(location.latitude, location.longitude)
-                map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
+                placeMarkerOnMap(currentLatLng)
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12.0f))
             }
         }
     }
+
+    private fun placeMarkerOnMap(location: LatLng) {
+        val markerOptions = MarkerOptions().position(location)
+        markerOptions.icon(BitmapDescriptorFactory.fromBitmap(
+                BitmapFactory.decodeResource(resources, R.mipmap.ic_user_location)))
+        val titleStr = getAddress(location)
+        markerOptions.title(titleStr)
+
+        map.addMarker(markerOptions)
+    }
+
+    private fun getAddress(latLng: LatLng): String {
+        val geocoder: Geocoder
+        val addresses: List<Address>
+        geocoder = Geocoder(this, Locale.getDefault())
+
+        addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1)
+
+        val address = addresses[0].getAddressLine(0)
+        val city = addresses[0].locality
+        val state = addresses[0].adminArea
+        val country = addresses[0].countryName
+        val postalCode = addresses[0].postalCode
+        return address
+    }
+
 
     override fun onMarkerClick(p0: Marker?) = false
 }
